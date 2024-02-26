afa1d9ce5a885e94923fd3a291cb1c58f8d946d9
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-02-26 09:02:35 +0200

    Blogs

---
+++ db.sql
@@ -0,0 +1,15 @@
+/*
+CREATE TABLE posts (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(255) NOT NULL
+);
+*/
+
+
+/*
+INSERT INTO posts
+(title)
+VALUES
+("My First Blog Post"),
+("My Second Blog Post");
+*/
