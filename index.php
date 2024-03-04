ef79459f81e1a64e8e9f5d1e916de95f40ab00cd
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-04 15:19:36 +0200

    added

--- index.php
+++ index.php
@@ -4,10 +4,24 @@ require "Database.php";
 $config = require ("config.php");
 
 echo "<center>Welcome to My Blog! :D</center>";
+echo "<h1>Posts</h1>";
+
+echo "<form>";
+echo "<input name='id' />";
+echo "<button>Submit</button>";
+echo "</form>";
+
+
+$query = "SELECT * FROM posts";
+if(isset($_GET["id"]) && $_GET["id"] !="") {
+    $id = $_GET["id"];
+    $query = "SELECT * FROM posts WHERE id=$id";
+}
+
 
 $db = new Database($config);
 $posts = $db
-            ->execute("SELECT * FROM posts")
+            ->execute($query)
             ->fetchAll();
 
 echo "<ul>";
