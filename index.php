a405274d45f63c3edc0fb53f1b2d56c7d2c6a93e
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-15 09:05:03 +0200

    added router

--- index.php
+++ index.php
@@ -1,16 +1,2 @@
 <?php
-require "functions.php";
-
-$url_array = parse_url($_SERVER["REQUEST_URI"]);
-$url = $url_array["path"];
-
-if($url == "/") {
-    require "controllers/index.php";
-} else if($url == "/about") {
-    require "controllers/about.php";
-} else if($url == "/story") {
-    require "controllers/story.php";
-} else {
-    http_response_code(404);
-    require "controllers/404.php";
-}
+require "router.php";
---
+++ routes.php
@@ -0,0 +1,6 @@
+<?php
+$routes = [
+    "/" => "controllers/index.php",
+    "/about" => "controllers/about.php",
+    "/story" => "controllers/story.php"
+];
